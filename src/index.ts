40f93284487cd2e007b87085a6575c0ec3301de2
CJskii <czareczarek3@gmail.com>
2024-09-28 11:17:19 +0100

    feat: bot startup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello");
+import TelegramBot from "node-telegram-bot-api";
+import "dotenv/config";
+
+const token = process.env.TELEGRAM_BOT_API_KEY;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(
+    chatId,
+    "Welcome to SeaCommander Bot! Use /play to start the game."
+  );
+});
+
+bot.onText(/\/play/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Get ready to play SeaCommander!", {
+    reply_markup: {
+      inline_keyboard: [[{ text: "Play Now!", callback_data: "play" }]],
+    },
+  });
+});
+
+console.log("SeaCommander bot is running...");
